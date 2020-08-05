e8d606bf12a9a552231458fdeb0818449983e67e
Italo Rangel <italopenaforte@gmail.com>
2020-08-05 08:07:50 -0300

    fix format

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -7,30 +7,30 @@ import './styles.css';
 export default function TeacherItem () {
     return (
         <article className="teacher-item">
-<header>
-    <img src="https://avatars3.githubusercontent.com/u/7786881?s=460&u=e64a8f24b1ba95eb82f283be8ab90892e40c5465&v=4" alt="Ítalo Rangel"/>
-    <div>
-        <strong>Ítalo Rangel</strong>
-        <span>Biologia</span>
-    </div>
-</header>
-<p>
-    Entusiata das melhores tecnologias de biologia avançada.
-    <br/><br/>
-    Apaxionado por clonagem de animal
-</p>
+            <header>
+                <img src="https://avatars3.githubusercontent.com/u/7786881?s=460&u=e64a8f24b1ba95eb82f283be8ab90892e40c5465&v=4" alt="Ítalo Rangel"/>
+                <div>
+                    <strong>Ítalo Rangel</strong>
+                    <span>Biologia</span>
+                </div>
+            </header>
+            <p>
+                Entusiata das melhores tecnologias de biologia avançada.
+                <br/><br/>
+                Apaxionado por clonagem de animal
+            </p>
 
-<footer>
-    <p>
-        Preço/Hora
-        <strong>R$ 80,00</strong>
-    </p>
-    <button type="button">
-        <img src={whatsAppIcon} alt="WhatsApp"/>
-        Entrar em contato
-    </button>
+            <footer>
+                <p>
+                    Preço/Hora
+                    <strong>R$ 80,00</strong>
+                </p>
+                <button type="button">
+                    <img src={whatsAppIcon} alt="WhatsApp"/>
+                    Entrar em contato
+                </button>
 
-</footer>
-</article>
+            </footer>
+        </article>
     )
 }
